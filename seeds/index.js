aa46759c8b14ace0f8d5c1f343f7422e21748f45
Diogo Guedes <diogofoguedes@hotmail.com>
2021-09-03 10:47:41 +0100

    Error handling and some changes regarding styling

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price= Math.floor(Math.random() * 20 ) +10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/2060184',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit cum repudiandae corrupti, aspernatur mollitia voluptas ea accusantium odit enim consequatur quo culpa alias commodi vitae ducimus facilis eum eaque voluptatum.',
+      price: price
     });
     await camp.save();
   }
@@ -28,3 +32,5 @@ const seedDB = async () => {
 seedDB().then(() => {
   mongoose.connection.close();
 });
+
+
